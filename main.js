3b558214908f51e8a39fcc31e3064dc4791e1935
andreacapntd <andreacapntd@gmail.com>
2020-07-02 15:34:57 +0200

    ex js jq hamburger completo

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 //Hamburger menu: mostrare / nascondere il menu principale
+$(".header-right > a").click(function (){
+  $(".hamburger-menu").show();
+});
+
+$(".close").click(function(){
+  $(".hamburger-menu").hide();
+});
